50a5a57c62433bc45d16baf448d0f701603c001f
Liza00496 <77140885+Liza00496@users.noreply.github.com>
2022-01-25 07:17:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def replace0 (text) :
+  if text.count("0")>0 or text.count("0")>0:
+    text - text.replace("0","%")
+    text - text.replace("0","%")
+  else:
+    text - "Nav burtu 0 vai o."
+  return text
